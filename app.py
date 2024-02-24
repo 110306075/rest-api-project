b37cf4182b7a24cde0adfc857202da75e30b37ab
shenghung <shenghung619@gmail.com>
2024-02-24 14:08:44 +0800

    add env variable (posrasql)

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_smorest import Api
 from db import db
 import secrets
 import models
+import os
+
+from dotenv import load_dotenv
 
 from flask_jwt_extended import JWTManager
 
@@ -22,6 +25,12 @@ from flask_migrate import Migrate
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
+
+
+
+
+
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -31,7 +40,7 @@ def create_app(db_url=None):
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
